a82f595b65cb468fce3589bab3645619038521f2
Oleg <w.shevchuk.o@gmail.com>
2020-12-03 18:32:35 +0200

    Starting frequency control operators

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -2,13 +2,10 @@ import someImage from "../assets/resource/images/DesktopWallpaper.jpg"
 import { component_1 } from "@components/component_one";
 import '../css/style.css';
 import '../scss/style.scss';
-import json from "../js/JSON/test.json";
 
-console.log("JSON file content:", json);
 document.body.appendChild(component_1());
 const img = document.createElement("img");
 img.src = someImage;
-console.log(img);
 document.body.appendChild(img);
 
 // new JS featuters
@@ -62,7 +59,7 @@ promiseOnEL.then((event: KeyboardEvent) => {
 })();
 
 import { fromEvent, of, from, timer, interval, range, empty, throwError } from 'rxjs';
-import { switchMap, debounceTime, filter, ignoreElements } from 'rxjs/operators'
+import { switchMap, debounceTime, filter, ignoreElements, first, last, single, find, debounce } from 'rxjs/operators'
 const observable = fromEvent(input_2, 'input');
 observable.pipe(
   debounceTime(600),
@@ -150,8 +147,14 @@ observable_from_Promise.subscribe(
   () => console.log("Completed observable_from_Promise !")
 );
 
-// const observable_timer = timer(0, 400)
-// const observable_interval = interval(500);
+const observable_timer = timer(0, 400)
+const observable_interval = interval(100);
+// observable_interval.subscribe(
+//   (nextData) => console.log(nextData),
+//   (errorData) => console.warn(errorData),
+//   () => console.log("Completed observable_interval!")
+// );
+
 // observable_timer.subscribe(
 //   (nextData) => console.log(nextData),
 //   (errorData) => console.warn(errorData),
@@ -186,8 +189,89 @@ const rengeProcessing = observable_range.pipe(
     return value > 5;
   })
 ).subscribe(
-  (value) => console.log('rengeProcessing', value)
+  (value) => console.log('Filtering renge', value)
 )
+
+const rengeProcessing_first = observable_range.pipe(
+  first((value: number) => {
+    return (value > 6);
+  }, 0)
+).subscribe(
+  (value) => console.log('Filter first renge:', value)
+)
+
+const clicks: Observable<Event> = fromEvent(document, 'click');
+const result = clicks.pipe(first(
+  (ev: Event) => {
+    const element = ev.target as HTMLElement;
+    return element.tagName === 'INPUT';
+  }
+));
+result.subscribe(x => console.log("Input first click: ", x));
+
+const rengeProcessing_last = observable_range.pipe(
+  last((value: number) => {
+    return (value < 5);
+  }, 0)
+).subscribe(
+  (value) => console.log('Filter last renge:', value)
+)
+
+const rengeProcessing_single = observable_range.pipe(
+  single((value: number, index, SourseObservable) => {
+    return (value < 5);
+  })
+).subscribe(
+  (value) => console.log('Filter last renge:', value),
+  (error) => console.warn(error)
+)
+
+
+const rengeProcessing_single_2 = observable_range.pipe(
+  single((value: number, index, SourseObservable) => {
+    return (value === 5);
+  })
+).subscribe(
+  (value) => console.log('Filter single_2 renge:', value),
+  (error) => console.warn(error)
+)
+
+const fromProcessing_single_3 = from([1, 3, 5, 67, 5, 92]).pipe(
+  single((value: number, index, SourseObservable) => {
+    return (value === 5);
+  })
+).subscribe(
+  (value) => console.log('Filter single_3 renge:', value),
+  (error) => console.warn('Filter single_3 renge:', error)
+)
+
+const rengeProcessing_single_4 = observable_range.pipe(
+  single((value: number, index, SourseObservable) => {
+    return (value === 100);
+  })
+).subscribe(
+  (value) => console.log('Filter single_4 renge:', value),
+  (error) => console.warn('Filter single_4 renge:', error)
+)
+
+const rengeProcessing_find = observable_range.pipe(
+  find((value: number) => {
+    return (value === 6);
+  })
+).subscribe(
+  (value) => console.log('Filter find renge:', value),
+  (error) => console.warn('Filter find renge:', error)
+)
+
+const fromProcessing_find_2 = from([1, 3, 5, 67, 5, 92]).pipe(
+  find((value: number) => {
+    return (value === 11);
+  })
+).subscribe(
+  (value) => console.log('Filter find_2 renge:', value),
+  (error) => console.warn('Filter find_2 renge:', error)
+)
+
 
 // фільтрація всіх значень з видачою лише статусу роботи Observable
 const rengeProcessing_2 = observable_range.pipe(
@@ -196,4 +280,20 @@ const rengeProcessing_2 = observable_range.pipe(
   (nextData) => console.log("rengeProcessing_2:", nextData),
   (errorData) => console.warn(errorData),
   () => console.log("Completed rengeProcessing_2 !")
+)
+
+const rengeProcessingDebounce_interval = observable_range.pipe(
+  debounce(() => interval(500))
+).subscribe(
+  (nextData) => console.log("rengeProcessingDebounce:", nextData),
+  (errorData) => console.warn("rengeProcessingDebounce", errorData),
+  () => console.log("Completed rengeProcessingDebounce!")
+)
+
+const rengeProcessingDebounce_timer = interval(1000).pipe(
+  debounce(() => timer(999))
+).subscribe(
+  (nextData) => console.log("rengeProcessingDebounce timer:", nextData, Date.now()),
+  (errorData) => console.warn("rengeProcessingDebounce timer", errorData),
+  () => console.log("Completed rengeProcessingDebounce timer!")
 )
